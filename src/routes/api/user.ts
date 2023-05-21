bbf8d992dbb54a4dae8815ceadc81dbc23367ea9
Minh Doan <minhdpc@gmail.com>
2023-05-21 22:35:52 +0700

    Remove un-need code

--- src/routes/api/user.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { UserController } from "src/controllers";
-
-const router = Router();
-
-router.get("/list", UserController.list);
-
-export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,9 @@
 import { Router } from "express";
 import helmet from "helmet";
-import { WelcomeController } from "src/controllers";
 import { allowCors } from "src/middlewares";
-import api from "./api";
 
 const routes = Router();
 
 routes.use(helmet(), allowCors());
-
-routes.get("/", WelcomeController.index);
-
-routes.use("/api", api);
 
 export default routes;
